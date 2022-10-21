014333f075298b02d6de396cd39bd2cfb20c0d57
NorihoKawasaki <115777689+NorihoKawasaki@users.noreply.github.com>
2022-10-21 14:31:33 +0900

    editForm.phpを編集

--- editForm.php
+++ editForm.php
@@ -1,15 +1,24 @@
 <?php 
-//DB接続
+//< DB接続 >
 include_once("./dbconnect.php");
+include_once("./functions.php");
 
-//選択されたidの取得
-$id = $_GET["id"];
-var_dump($_GET);
+//< 選択されたidの取得 >
+$id = $_GET['id'];
+// var_dump($_GET);
 
-//編集するデータを取得
+//< 編集するデータを取得 >
 //1. SQL作成(id取得) AUTO-INCREMENTが設定されているのでidを取得することによって編集が可能になる
 $sql = "SELECT * FROM records WHERE id = :id";
-//
+//2. SQL実行の準備
+$stmt = $pdo -> prepare($sql);
+$stmt -> bindParam(":id", $id, PDO::PARAM_INT); //値の設定
+//3. SQLの実行
+$stmt -> execute();
+//4．実行結果の取得、★一つのデータだからrecord変数
+$record = $stmt -> fetch();
+
+// var_dump($record);
 
 ?>
 
@@ -39,23 +48,24 @@ $sql = "SELECT * FROM records WHERE id = :id";
       <p class="alert alert-success" role="alert">編集フォーム</p>
       <div class="form-group">
         <label for="date">日付</label>
-        <input type="date" class="form-control" id="date" name="date">
+        <!-- value追加 -->
+        <input type="date" class="form-control" id="date" name="date" value="<?php echo h($record["date"]); ?>">
       </div>
       <div class="form-group">
         <label for="title">タイトル</label>
-        <input type="text" class="form-control" id="title" name="title">
+        <input type="text" class="form-control" id="title" name="title" value="<?php echo h($record["title"]); ?>">
       </div>
       <div class="form-group">
         <label for="amount">金額</label>
-        <input type="number" class="form-control" id="amount" name="amount">
+        <input type="number" class="form-control" id="amount" name="amount" value="<?php echo h($record["amount"]); ?>">
       </div>
       <div class="form-group">
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="type" id="income">
+          <input class="form-check-input" type="radio" name="type" id="income" value="0"<?php echo h($record["type"]) == 0 ? "checked" : "" ; ?>>
           <label class="form-check-label" for="income">収入</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="type" id="spending">
+          <input class="form-check-input" type="radio" name="type" id="spending" value="1"<?php echo h($record["type"]) == 1 ? "checked" : "" ; ?>>
           <label class="form-check-label" for="spending">支出</label>
         </div>
       </div>
